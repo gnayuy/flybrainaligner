fb56dc423e02bd05d76e948c382be00baccaabdf
Yang Yu <yuy@localhost.localdomain>
2017-03-24 14:06:27 -0400

    add comments for genAlignScript.sh

--- pipelines/misc/genAlignScript.sh
+++ pipelines/misc/genAlignScript.sh
@@ -1,10 +1,26 @@
+#!/bin/sh
 
+# developed by Yang Yu (yuy@janelia.hhmi.org) 03/23/2017
+# genrate the alignment script
+#
 
-# for i in /nrs/scicompsoft/yuy/registration/images/*/alignCmd.sh; do qsub -pe batch 32 -l broadwell=true -j y -b y -cwd -V $i; done
-# qstat
+# Usages:
+###
+# Example 1 (for single brain): 
+# $mkdir /nrs/scicompsoft/yuy/registration/images/VT005002_AE_01-20131024_33_I1
+# $mkdir /nrs/scicompsoft/yuy/registration/images/VT005002_AE_01-20131024_33_I1/images
+# $cp VT005002_AE_01-20131024_33_I1_*.nrrd /nrs/scicompsoft/yuy/registration/images/VT005002_AE_01-20131024_33_I1/images
+# $sh genAlignScript.sh /nrs/scicompsoft/yuy/registration/images/VT005002_AE_01-20131024_33_I1 32
+# $qsub -pe batch 32 -l broadwell=true -j y -b y -cwd -V /nrs/scicompsoft/yuy/registration/images/VT005002_AE_01-20131024_33_I1/alignCmd.sh
+###
+# Example 2 (for multiple brains):
+# $for i in /nrs/scicompsoft/yuy/registration/images/*.nrrd; do j=${i%*_*}; mkdir $j; mkdir $j/images; done
+# $for i in /nrs/scicompsoft/yuy/registration/images/*.nrrd; do j=${i%*_*}; mv $j* $j/images/; done
+# $for i in /nrs/scicompsoft/yuy/registration/images/*; do sh genAlignScript.sh $i 32; done
+# $for i in /nrs/scicompsoft/yuy/registration/images/*/alignCmd.sh; do qsub -pe batch 32 -l broadwell=true -j y -b y -cwd -V $i; done
+# $qstat
 
-
-
+#
 INPUTDIR=$1
 THREAD=$2
 OUT=$3
